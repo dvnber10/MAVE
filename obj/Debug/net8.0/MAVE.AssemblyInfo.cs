770045ad3e5bb289482840acb493283e5e8992ea
Subblack <osfel15102003@gmail.com>
2024-03-26 20:10:38 -0500

    Organizacion de Carpetas

--- obj/Debug/net8.0/MAVE.AssemblyInfo.cs
+++ obj/Debug/net8.0/MAVE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAVE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80ac2d7ee0fc2f003ad0d1200600f7bf4b46f2b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1edad86b4d42b0e00606bbd078b305be6a3772e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAVE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAVE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
